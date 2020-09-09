484cf102f94257c908889e66103de3956de9e9c5
bhand <sushbhand123@gmail.com>
2020-09-09 21:54:39 +0200

    Initial Commit

---
+++ CNNModel/model.py
@@ -0,0 +1,25 @@
+# Imports
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Design Network
+class ImageClassifierOnCIFAR(nn.Module):
+    def __init__(self, in_channel=3, num_class=10):
+        super(ImageClassifierOnCIFAR, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=6, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=num_class)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.reshape(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
