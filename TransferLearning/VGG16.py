f3b04cc228fac8f3d1e07859abba548c013798a9
bhand <sushbhand123@gmail.com>
2020-09-14 15:27:48 +0200

    VGG16 - initial commit

---
+++ TransferLearning/VGG16.py
@@ -0,0 +1,58 @@
+# Imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as Dataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision
+from helper import *
+
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper Parameters
+in_channel = 3
+mum_classes = 10
+learning_rate = 1e-3
+batch_size = 1024
+epoch = 5
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+# Load Pre-trained model and retrain it
+model = torchvision.models.vgg16(pretrained=True)
+model.avgpool = Identity()
+model.classifier = nn.Linear(512, 10)
+model.to(device)
+
+# Load Data
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = Dataset.CIFAR10(root='dataset/', train=True, download=True, transform=transform)
+trainloader = DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True)
+
+testset = Dataset.CIFAR10(root='dataset/', train=False, download=True, transform=transform)
+testloader = DataLoader(dataset=testset, batch_size=batch_size, shuffle=True)
+
+
+# Set loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
+train(model=model, optimizer=optimizer, loss_function=criterion, dataloader=trainloader, epoch=epoch)
+print("Checking accuracy on train data.")
+check_accuracy(trainloader, model)
+print("Checking accuracy in test data.")
+check_accuracy(testloader, model)
